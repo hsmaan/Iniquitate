86994af9af8ae559f1461ab09f5b38b9a4a199e0
hsmaan <hmaan@uoguelph.ca>
2021-12-14 03:20:50 -0500

    preserving raw counts for seurat_integrate step

--- workflow/src/python/utils/seurat_integrate.py
+++ workflow/src/python/utils/seurat_integrate.py
@@ -89,6 +89,9 @@ class SeuratIntegrate:
         # Reappend original obs columns
         integrated_adata.obs = self.adata.obs
         
+        # Reappend raw data layer 
+        integrated_adata.layers["raw"] = self.adata.layers["raw"]
+        
         # Return integrated AnnData object
         return integrated_adata
     
