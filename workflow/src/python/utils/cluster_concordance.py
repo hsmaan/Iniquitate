99e92b6917e5062af490709e5138403df02f7e8a
hsmaan <hmaan@uoguelph.ca>
2021-12-28 23:38:54 -0500

    removed scvi from clustering concordance src function

--- workflow/src/python/utils/cluster_concordance.py
+++ workflow/src/python/utils/cluster_concordance.py
@@ -4,7 +4,7 @@ from sklearn import metrics
 
 def cluster_concordance(adata):
     # Pull out clustering values per subset
-    methods = ["harmony", "scvi", "bbknn", "scanorama", "seurat", "liger"]
+    methods = ["harmony", "bbknn", "scanorama", "seurat", "liger"]
     adata_subsets = []
     for method in methods:
         adata_subsets.append(adata[adata.obs["integration_method"] == method])
