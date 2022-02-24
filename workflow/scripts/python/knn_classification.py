8fc4997d7db0e4f03f50eaaaebb9852cdb760302
hsmaan <hmaan@uoguelph.ca>
2022-02-23 19:51:03 -0500

    added complete script for knn classification per method

--- workflow/scripts/python/knn_classification.py
+++ workflow/scripts/python/knn_classification.py
@@ -5,6 +5,11 @@ import sys
 import scanpy as sc
 import anndata as ann
 import numpy as np
+import pandas as pd 
+from sklearn.neighbors import NearestNeighbors
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import classification_report
 
 def main(h5ad_loc, save_loc, dataset_name, rep):
     # Load h5ad file 
@@ -23,7 +28,72 @@ def main(h5ad_loc, save_loc, dataset_name, rep):
         adata_method_sub.append(
             adata_sub
         )
+
+    # Determine KNN accuracy for each batch-correction method
+    precision_scores = []
+    recall_scores = []
+    f1_scores = []
+    supports = []
+    celltypes = []
+    for adata_sub in adata_method_sub:
+        # Split testing and training data in stratified manner (70/30)
+        X = adata_sub.obsm["X_kmeans"]
+        y = adata.obs["celltype"].__array__()
+        X_train, X_test, y_train, y_test = train_test_split(
+            X, y, stratify=y, test_size=0.7, random_state=42
+        )
         
+        # Train k-nearest neighbors classifier with k=15 and predict on test data
+        knn = KNeighborsClassifier(
+            n_neighbors=15
+        )
+        knn.fit(X_train, y_train)
+        y_pred = knn.predict(X_test)
+        
+        # Get classification report and subset for only relevant columns 
+        class_report_dict = classification_report(
+            y_test, y_pred, output_dict=True
+        )
+        class_report_df = pd.DataFrame(class_report_dict)
+        class_report_df = class_report_df.iloc[:, :-3]
+        
+        # Append appropriate values to lists
+        precision_scores.append(class_report_df.loc["precision"].values)
+        recall_scores.append(class_report_df.loc["recall"].values)
+        f1_scores.append(class_report_df.loc["f1-score"].values)
+        supports.append(class_report_df.loc["support"].values)
+        celltypes.append(class_report_df.columns.values)
+        
+    # Tile method values to have same length as scores (one for each celltype)
+    methods_tiled = np.tile(methods, len(precision_scores[0]))
+    
+    # Concatenate scores and celltypes 
+    precision_scores_concat = np.concatenate(precision_scores)
+    recall_scores_concat = np.concatenate(recall_scores)
+    f1_scores_concat = np.concatenate(f1_scores)
+    supports_concat = np.concatenate(supports)
+    celltypes_concat = np.concatenate(celltypes)
+    
+    # Create summary dataframe for classification statistics and save
+    classification_summary_df = pd.DataFrame({
+        "Dataset": dataset_name,
+        "Number of batches downsampled": num_batches_ds,
+        "Number of celltypes downsampled": num_celltypes_ds,
+        "Proportion downsampled": prop_ds,
+        "Replicate": rep,
+        "Method": methods_tiled,
+        "Celltype": celltypes_concat,
+        "Precision": precision_scores_concat,
+        "Recall": recall_scores_concat,
+        "F1-score": f1_scores_concat,
+        "Support": supports_concat,
+        "Mean KNN F1-score": np.mean(f1_scores_concat)
+    })
+    classification_summary_df.to_csv(
+        save_loc,
+        index=False,
+        sep="\t"
+    )
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
