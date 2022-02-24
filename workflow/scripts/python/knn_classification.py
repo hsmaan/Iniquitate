2ae9b57a7bc3d30ae0b14c280670a1323e072f4f
hsmaan <hmaan@uoguelph.ca>
2022-02-23 20:03:42 -0500

    added knn classification rule to snakefile

--- workflow/scripts/python/knn_classification.py
+++ workflow/scripts/python/knn_classification.py
@@ -6,7 +6,7 @@ import scanpy as sc
 import anndata as ann
 import numpy as np
 import pandas as pd 
-from sklearn.neighbors import NearestNeighbors
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import classification_report
