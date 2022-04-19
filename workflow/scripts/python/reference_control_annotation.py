be4600fdc34c44a4e4b25a2e7488d6f78193ce4f
hsmaan <hmaan@uoguelph.ca>
2022-04-19 13:42:37 -0400

    updated query to ref control to use hvgs

--- workflow/scripts/python/reference_control_annotation.py
+++ workflow/scripts/python/reference_control_annotation.py
@@ -10,20 +10,21 @@ from utils import cross_data_knn
 
 def main(h5ad_loc, ref_h5_loc, save_loc):
     # Load the seurat reference mapped h5ad file 
-    query_h5ad = sc.read_h5ad(h5ad_loc)
+    query_h5ad_full = sc.read_h5ad(h5ad_loc)
+    query_h5ad = query_h5ad_full.raw.to_adata() # Use sctransformed counts
     
     # Load the reference h5ad file
     ref_h5ad = sc.read_h5ad(ref_h5_loc)
     
     # Get the intersection of the genes in the query and reference h5ad files
-    query_genes = set(query_h5ad.raw.var._index.__array__()) # query sct data is stored in raw
+    query_genes = set(query_h5ad.var._index.__array__())
     ref_genes = set(ref_h5ad.var._index.__array__())
     common_genes_list = list(ref_genes.intersection(query_genes))
 
     # Get indices of the common genes in both query and ref h5ad files
     query_sct_gene_indices = np.where(
         np.isin(
-            query_h5ad.raw.var._index.__array__(), 
+            query_h5ad.var._index.__array__(), 
             common_genes_list
         )
     )[0]
@@ -34,17 +35,25 @@ def main(h5ad_loc, ref_h5_loc, save_loc):
         )
     )[0]
 
+    # Subset anndata objects for the common genes
+    query_h5ad_sub = query_h5ad[:, query_sct_gene_indices]
+    ref_h5ad_sub = ref_h5ad[:, ref_sct_gene_indices]
+    
+    # Get highly variable gene subsets of both jointly and return indices 
+    query_ref_sub_concat = ann.AnnData.concatenate(query_h5ad_sub, ref_h5ad_sub)
+    sc.pp.highly_variable_genes(query_ref_sub_concat, flavor="seurat", n_top_genes=2500)
+    hvg_indices = query_ref_sub_concat.var["highly_variable"].__array__()
 
-    # Get the SCTransformed subsets for both the query and reference 
-    query_sct = query_h5ad.raw.X.toarray() # SCT counts are stored in the raw layer
-    ref_sct = ref_h5ad.X.toarray()
+    # Get the SCTransformed data subsets for both the query and reference (common gene subsets)
+    query_sct = query_h5ad_sub.X.toarray()
+    ref_sct = ref_h5ad_sub.X.toarray()
 
-    # Subset for the indices of the common genes
-    query_sct_subset = query_sct[:, query_sct_gene_indices]
-    ref_sct_subset = ref_sct[:, ref_sct_gene_indices]
+    # Subset for the indices of the hvg genes
+    query_sct_hvg_subset = query_sct[:, hvg_indices]
+    ref_sct_hvg_subset = ref_sct[:, hvg_indices]
     
     # Get the (1) nearest neighbors for the reference data within the query data
-    query_1_nn = cross_data_knn(query_sct_subset, ref_sct_subset, 1)
+    query_1_nn = cross_data_knn(query_sct_hvg_subset, ref_sct_hvg_subset, 1)
     
     # Get the celltypes (both l1 and l2) corresponding to the nearest neighbors for the reference data
     ref_celltypes_l1 = ref_h5ad.obs["celltype.l1"][query_1_nn.flatten()].__array__()
@@ -55,11 +64,8 @@ def main(h5ad_loc, ref_h5_loc, save_loc):
     query_h5ad.obs["baseline.knn.l2"] = ref_celltypes_l2
 
     # Change colnames of query var to not collide with h5ad writing in anndata
+    query_h5ad.var["gene_name"] = query_h5ad.var._index 
     query_h5ad.var = query_h5ad.var.drop(query_h5ad.var.columns[0], axis=1)
-    query_h5ad.var.columns = ["gene_name"]
-
-    # Remove raw layer from query h5ad file to avoid collision with h5ad writing in anndata
-    query_h5ad.raw = None
     
     # Save the query h5ad file with baseline annotations
     query_h5ad.write_h5ad(
