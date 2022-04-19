604be54df2618b11abe4a4836f0394723931b0b7
hsmaan <hmaan@uoguelph.ca>
2022-04-18 20:57:04 -0400

    added initial steps for reformatting figure 4 analysis

--- workflow/analysis/R/10_Iniq_Real_Datasets_Fig_4_Analysis_Plots.R
+++ workflow/analysis/R/10_Iniq_Real_Datasets_Fig_4_Analysis_Plots.R
@@ -34,84 +34,184 @@ kev_palette <- c(
   "darkorange4", "brown"
 )
 
+##### Analysis of PBMC 2 batch imbalanced data vs balanced data ##### - 
+#### with respect to ARI and other integration metrics 
+
 # Change to results dir for lowcap modified data 
 setwd("../../../results/lowcap_modified/")
-
-##### Analysis of PBMC 2 batch imbalanced data vs balanced data #####
 
 # Load in and concatenate celltype imbalance summary files
 setwd("celltype_imbalance_summaries")
 cimba_files <- list.files()
-cimba_files <- grep(
+pbmc_2_batch_imba_cimba_files <- grep(
   "pbmc_2_batch",
   cimba_files,
   value = TRUE
 )
-cimba_loaded <- lapply(cimba_files, fread)
-cimba_concat <- Reduce(rbind, cimba_loaded)
+pbmc_2_batch_imba_cimba_loaded <- lapply(
+  pbmc_2_batch_imba_cimba_files, 
+  fread
+)
+pbmc_2_batch_imba_cimba_concat <- Reduce(
+  rbind, 
+  pbmc_2_batch_imba_cimba_loaded
+)
 gc()
 
 # Load in and concatenate full imbalance summary files 
-setwd("imbalance_summaries/")
+setwd("../imbalance_summaries/")
 imba_files <- list.files()
-imba_files <- grep(
+pbmc_2_batch_imba_imba_files <- grep(
   "pbmc_2_batch",
   imba_files,
   value = TRUE
 )
-imba_loaded <- lapply(imba_files, fread)
-imba_concat <- Reduce(rbind, imba_loaded)
+pbmc_2_batch_imba_imba_loaded <- lapply(
+  pbmc_2_batch_imba_imba_files, 
+  fread
+)
+pbmc_2_batch_imba_imba_concat <- Reduce(
+  rbind, 
+  pbmc_2_batch_imba_imba_loaded
+)
 gc()
 
 # Load in and concatenate the clustering summary results 
 setwd("../clustering_summaries/")
 clus_files <- list.files()
-clus_files <- grep(
+pbmc_2_batch_imba_clus_files <- grep(
   "pbmc_2_batch",
   clus_files,
   value = TRUE
 )
-clus_loaded <- lapply(clus_files, fread)
-clus_concat <- Reduce(rbind, clus_loaded)
+pbmc_2_batch_imba_clus_loaded <- lapply(
+  pbmc_2_batch_imba_clus_files, 
+  fread
+)
+pbmc_2_batch_imba_clus_concat <- Reduce(
+  rbind, 
+  pbmc_2_batch_imba_clus_loaded
+)
 gc()
 
-# Load in and concatenate clustering concordance summaries 
-setwd("../clustering_concord_summaries/")
-clus_concord_files <- list.files()
-clus_concord_files <- grep(
+# Load in relatedness metric summary 
+setwd("../relatedness_results/")
+relate_files <- list.files()
+pbmc_2_batch_imba_relate_file <- grep(
   "pbmc_2_batch",
-  clus_concord_files,
+  relate_files,
   value = TRUE
 )
-clus_concord_loaded <- lapply(clus_concord_files, fread)
-clus_concord_concat <- Reduce(rbind, clus_concord_loaded)
-gc()
+pbmc_2_batch_imba_relate_loaded <- fread(pbmc_2_batch_imba_relate_file)
 
-# Load in and concatenate dge concordance summaries
-setwd("../dge_concord_stats/")
-dge_files <- list.files()
-dge_files <- grep(
-  "pbmc_2_batch",
-  dge_files,
+# Change to results dir for control data 
+setwd("../../control/")
+
+# Load in and concatenate celltype imbalance summary files
+setwd("celltype_imbalance_summaries")
+cimba_files <- list.files()
+pbmc_2_batch_bal_cimba_files <- grep(
+  "pbmc_2_batch_base_balanced",
+  cimba_files,
   value = TRUE
 )
-dge_loaded <- lapply(dge_files, fread)
-dge_concat <- Reduce(rbind, dge_loaded)
+pbmc_2_batch_bal_cimba_loaded <- lapply(
+  pbmc_2_batch_bal_cimba_files, 
+  fread
+)
+pbmc_2_batch_bal_cimba_concat <- Reduce(
+  rbind, 
+  pbmc_2_batch_bal_cimba_loaded
+)
 gc()
 
-
-# Load in and concatenate knn classification summaries
-setwd("../knn_classification_reports/")
-knn_files <- list.files()
-knn_files <- grep(
-  "pbmc_2_batch",
-  knn_files,
+# Load in and concatenate full imbalance summary files 
+setwd("../imbalance_summaries/")
+imba_files <- list.files()
+pbmc_2_batch_bal_imba_files <- grep(
+  "pbmc_2_batch_base_balanced",
+  imba_files,
   value = TRUE
 )
-knn_loaded <- lapply(knn_files, fread)
-knn_concat <- Reduce(rbind, knn_loaded)
+pbmc_2_batch_bal_imba_loaded <- lapply(
+  pbmc_2_batch_bal_imba_files, 
+  fread
+)
+pbmc_2_batch_bal_imba_concat <- Reduce(
+  rbind, 
+  pbmc_2_batch_bal_imba_loaded
+)
 gc()
+
+# Load in and concatenate the clustering summary results 
+setwd("../clustering_summaries/")
+clus_files <- list.files()
+pbmc_2_batch_bal_clus_files <- grep(
+  "pbmc_2_batch_base_balanced",
+  clus_files,
+  value = TRUE
+)
+pbmc_2_batch_bal_clus_loaded <- lapply(
+  pbmc_2_batch_bal_clus_files, 
+  fread
+)
+pbmc_2_batch_bal_clus_concat <- Reduce(
+  rbind, 
+  pbmc_2_batch_bal_clus_loaded
+)
+gc()
+
+# Load in relatedness metric summary 
+setwd("../relatedness_results/")
+relate_files <- list.files()
+pbmc_2_batch_bal_relate_file <- grep(
+  "pbmc_2_batch_base_balanced",
+  relate_files,
+  value = TRUE
+)
+pbmc_2_batch_bal_relate_loaded <- fread(pbmc_2_batch_bal_relate_file)
 
 # Change to top level dir 
 setwd("../../..")
+
+### Fig 4A) - Analysis of PBMC 2 batch results with respect to integration
+### and measures of relatedness 
+
+# Merge and subset balanced clustering data to only include control experiments 
+imba_clus_merged_bal <- merge(
+  pbmc_2_batch_bal_clus_concat,
+  pbmc_2_batch_bal_imba_concat,
+  by = c(
+    "Number of batches downsampled",
+    "Number of celltypes downsampled",
+    "Proportion downsampled",
+    "Replicate"
+  )
+)
+imba_clus_merged_bal$type <- ifelse(
+  imba_clus_merged_bal$`Number of batches downsampled` == 0,
+  "Control",
+  ifelse(
+    imba_clus_merged_bal$`Proportion downsampled` == 0,
+    "Ablated",
+    "Downsampled"
+  )
+)
+imba_clus_merged_bal_control <- imba_clus_merged_bal[
+  imba_clus_merged_bal$type %in% c("Control")
+]
+
+# Merge imbalanced clustering data
+imba_clus_merged_imba <- merge(
+  pbmc_2_batch_imba_clus_concat,
+  pbmc_2_batch_imba_imba_concat,
+  by = c(
+    "Number of batches downsampled",
+    "Number of celltypes downsampled",
+    "Proportion downsampled",
+    "Replicate"
+  )
+)
+
+
 
