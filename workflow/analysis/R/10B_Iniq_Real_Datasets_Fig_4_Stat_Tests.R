45c4645dadbeb7b38a87f959dd3add568dcf90f3
hsmaan <hassaan.maan@mail.utoronto.ca>
2022-09-05 19:31:44 -0400

    added global stat test for relatedness and support - lowcap mod analysis

--- workflow/analysis/R/10B_Iniq_Real_Datasets_Fig_4_Stat_Tests.R
+++ workflow/analysis/R/10B_Iniq_Real_Datasets_Fig_4_Stat_Tests.R
@@ -298,4 +298,192 @@ fwrite(
   col.names = TRUE
 )
 
-# 
+### Test 2 - testing whether or not the proposed metrics - relative celltype 
+### support and minimum celltype center distance are significant in an ANOVA
+### test for their predictive value for F1-scores 
+
+# Define datasets and keywords to grep
+datasets <- c( 
+  "pbmc_2_batch",
+  "pbmc_4_batch",
+  "mouse_hindbrain_6_batch",
+  "peng_pdac_8_batch"
+)
+
+# Load relatedness metrics for all four datasets
+setwd("results/lowcap_modified/relatedness_results/")
+relatedness_files <- list.files()
+relatedness_loaded <- lapply(
+  datasets, function(x) {
+    file_grep <- grep(x, relatedness_files, value = TRUE)
+    file_loaded <- fread(file_grep)
+    return(file_loaded)
+  }
+)
+names(relatedness_loaded) <- datasets
+
+# Load in KNN classification results for all four datasets
+setwd("../knn_classification_reports/")
+knn_class_files <- list.files()
+knn_class_files_loaded <- lapply(
+  datasets, function(x) {
+    files_grep <- grep(x, knn_class_files, value = TRUE)
+    files_loaded <- lapply(
+      files_grep, fread
+    )
+    files_concat <- Reduce(rbind, files_loaded)
+    files_concat <- files_concat[
+      files_concat$Method != "liger"
+    ]
+    return(files_concat)
+  }
+)
+names(knn_class_files_loaded) <- datasets
+
+# Change to top level dir
+setwd("../../..")
+
+# Create function that will run an ANOVA test for relatedness across the 
+# different datasets analyzed 
+relatedness_anova <- function(
+    dataset, relatedness_df, knn_class_df
+  ) {
+  # Format relatedness df to take the minimum (distance) of each celltype 
+  # to another celltype in the dataset, while excluding self-matches 
+  relatedness_df_sub <- relatedness_df[
+    relatedness_df$`Celltype 1` != relatedness_df$`Celltype 2`
+  ]
+  relatedness_df_min <- relatedness_df_sub %>%
+    group_by(`Celltype 1`) %>% 
+    summarize(var = min(`PCA cosine dist`)) %>%
+    as.data.frame()
+  colnames(relatedness_df_min) <- c("Celltype", "Min PCA cosine dist")
+  
+  # Merge together relatedness and knn class df
+  relatedness_knn_class_merged <- merge(
+    relatedness_df_min,
+    knn_class_df,
+    by = c(
+      "Celltype"
+    )
+  )
+  
+  # Format column names for ANOVA test 
+  colnames(relatedness_knn_class_merged) <- plyr::mapvalues(
+    colnames(relatedness_knn_class_merged),
+    from = c(
+      "Method",
+      "Min PCA cosine dist",
+      "F1-score"
+    ),
+    to = c(
+      "method",
+      "min_pca_cos_dist",
+      "f1_score"
+    )
+  )
+  
+  
+  # Perform LM model fit - note we do not include celltype as it's obviously
+  # collinear with the mininum pca cosine distance - but in this case we are 
+  # considering this covariate as numeric and not categorical like celltype 
+  model_fit <- lm(
+    as.formula(
+      paste0(
+        "f1_score", 
+        "~",
+        "method+",
+        "min_pca_cos_dist"
+      )
+    ),
+    data = relatedness_knn_class_merged
+  )
+  anova_result <- anova(model_fit, test = "F")
+  anova_result_dt <- as.data.table(anova_result, keep.rownames = TRUE)
+  colnames(anova_result_dt)[1] <- "Covariate"
+  anova_result_dt$dataset_name <- dataset
+  anova_result_dt$metric <- "f1_score"
+  anova_result_dt$last_covariate <- "min_pca_cos_dist"
+  return(anova_result_dt)
+}
+
+# Get the ANOVA results across all methods for Minimum PCA cosine distance
+relatedness_anova_results <- mapply(
+  relatedness_anova,
+  dataset = datasets,
+  relatedness_df = relatedness_loaded,
+  knn_class_df = knn_class_files_loaded,
+  SIMPLIFY = FALSE
+)
+relatedness_anova_results_concat <- Reduce(rbind, relatedness_anova_results)
+fwrite(
+  relatedness_anova_results_concat,
+  "outs/lowcap_modified/results/10B_all_ds_anova_relatedness_results.tsv",
+  sep = "\t",
+  quote = FALSE,
+  row.names = FALSE,
+  col.names = TRUE
+)
+
+# Create function that will run an ANOVA test for celltype support across the 
+# different datasets analyzed 
+support_anova <- function(dataset, knn_class_df) {
+  # Add log of support to knn class df
+  knn_class_df$Log_support <- log2(knn_class_df$Support)
+  
+  # Format column names for ANOVA test 
+  colnames(knn_class_df) <- plyr::mapvalues(
+    colnames(knn_class_df),
+    from = c(
+      "Method",
+      "Log_support",
+      "F1-score"
+    ),
+    to = c(
+      "method",
+      "log_support",
+      "f1_score"
+    )
+  )
+  
+  # Perform LM model fit - note we do not include celltype as it's obviously
+  # collinear with the log celltype support - but in this case we are 
+  # considering this covariate as numeric and not categorical like celltype 
+  model_fit <- lm(
+    as.formula(
+      paste0(
+        "f1_score", 
+        "~",
+        "method+",
+        "log_support"
+      )
+    ),
+    data = knn_class_df
+  )
+  anova_result <- anova(model_fit, test = "F")
+  anova_result_dt <- as.data.table(anova_result, keep.rownames = TRUE)
+  colnames(anova_result_dt)[1] <- "Covariate"
+  anova_result_dt$dataset_name <- dataset
+  anova_result_dt$metric <- "f1_score"
+  anova_result_dt$last_covariate <- "log_support"
+  return(anova_result_dt)
+}
+
+# Get the ANOVA results across all methods for relative celltype support
+support_anova_results <- mapply(
+  support_anova,
+  dataset = datasets,
+  knn_class_df = knn_class_files_loaded,
+  SIMPLIFY = FALSE
+)
+support_anova_results_concat <- Reduce(rbind, support_anova_results)
+fwrite(
+  support_anova_results_concat,
+  "outs/lowcap_modified/results/10B_all_ds_anova_support_results.tsv",
+  sep = "\t",
+  quote = FALSE,
+  row.names = FALSE,
+  col.names = TRUE
+)
+
+
