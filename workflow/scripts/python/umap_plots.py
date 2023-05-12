294c0550aebaada89119e9296dd63b0ad8626de4
hsmaan <hassaan.maan@mail.utoronto.ca>
2023-05-12 16:59:31 -0400

    added umap plotting functionality + script

---
+++ workflow/scripts/python/umap_plots.py
@@ -0,0 +1,98 @@
+import argparse 
+import os 
+import sys 
+
+import numpy as np
+import pandas as pd
+import anndata as ann
+import scanpy as sc 
+
+from utils import Umap
+
+def main(h5ad_loc, save_loc, dataset_name, rep):
+    # Load h5ad file 
+    adata = sc.read_h5ad(h5ad_loc)
+    
+    # Get the umap coordinates for all the methods and create dictionary object
+    methods = ["bbknn", "harmony", "scanorama", "scvi", "seurat"]
+    umap_dict = {}
+    for method in methods:
+        adata_sub = adata[adata.obs["integration_method"] == method]
+        umap_dict[method] = adata_sub.obsm["X_umap"].__array__()
+        
+    # Get the leiden clustering for all the methods and create dictionary object
+    leiden_dict = {}
+    for method in methods:
+        adata_sub = adata[adata.obs["integration_method"] == method]
+        leiden_dict[method] = adata_sub.obs["leiden"].__array__()
+        
+    # Create a cell type dictionary object
+    celltype_dict = {}
+    for method in methods:
+        adata_sub = adata[adata.obs["integration_method"] == method]
+        celltype_dict[method] = adata_sub.obs["celltype"].__array__()
+        
+    # Create a batch dictionary object
+    batch_dict = {}
+    for method in methods:
+        adata_sub = adata[adata.obs["integration_method"] == method]
+        batch_dict[method] = adata_sub.obs["batch"].__array__()
+        
+    # Create a umap object for each subset of information
+    umap_leiden = Umap(
+        coords = umap_dict,
+        clustering = leiden_dict,
+        subset_name = "Clustering",
+    )
+    umap_celltype = Umap(
+        coords = umap_dict,
+        clustering = celltype_dict,
+        subset_name = "Cell-type",
+    )
+    umap_batch = Umap(
+        coords = umap_dict,
+        clustering = batch_dict,
+        subset_name = "Batch",
+    )
+    
+    # Plot each of the unap objects
+    umap_leiden.umap_df()
+    umap_leiden.umap_plot(show_plot=True)
+    umap_leiden.save_umap(
+        save_dir=save_loc + "_leiden.pdf",
+        dpi=300
+    )
+    
+    umap_celltype.umap_df()
+    umap_celltype.umap_plot(show_plot=True)
+    umap_celltype.save_umap(
+        save_dir=save_loc + "_celltype.pdf"",
+        dpi=300
+    )
+    
+    umap_batch.umap_df()
+    umap_batch.umap_plot(show_plot=True)
+    umap_batch.save_umap(
+        save_dir=save_loc + "_batch.pdf",
+        dpi=300
+    )
+    
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description = "Input and output files for UMAP plot generation"
+    )
+    parser.add_argument(
+        "--infile",
+        type = str,
+        help = "Path of integrated h5ad file"
+    )
+    parser.add_argument(
+        "--outfile",
+        type = str,
+        help = "Filepath for saving UMAP results from integrated h5ad file"
+    )
+    args = parser.parse_args()
+    main(
+        h5ad_loc = args.infile,
+        save_loc = args.outfile
+    )
---
+++ workflow/src/python/utils/umap.py
@@ -0,0 +1,119 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import colorcet as cc
+from utils.nat_sort import natural_sort
+
+
+class Umap:
+    """Class for plotting results of integration experiments"""
+
+    def __init__(self, coords, clustering, subset_name = None):
+        """
+        Args:
+            coords (dictionary): coordinates of umap in numpy format where
+                keys correspond to following integration methods -
+                'bbknn', 'harmony', 'scanorama', 'seurat', and 'scvi'.
+            clustering (dictionary): leiden or celltype clustering in numpy
+                format of integrated where keys correspond to following
+                integration methods - 'bbknn', 'harmony', 'scanorama',
+                'seurat', and 'scvi'.
+            subset_name (string): name of subset being utilized for clustering
+                comparisons (e.g. batch, celltype).
+        """
+        self.clustering_harmony = clustering.get("harmony")
+        self.clustering_scvi = clustering.get("scvi")
+        self.clustering_bbknn = clustering.get("bbknn")
+        self.clustering_scanorama = clustering.get("scanorama")
+        self.clustering_seurat = clustering.get("seurat")
+    
+        self.umap_harmony = coords.get("harmony")
+        self.umap_scvi = coords.get("scvi")
+        self.umap_bbknn = coords.get("bbknn")
+        self.umap_scanorama = coords.get("scanorama")
+        self.umap_seurat = coords.get("seurat")
+        
+        if subset_name is not None:
+            self.subset_name = subset_name
+        else:
+            self.subset_name = "Subset"
+            
+        sns.set_style("ticks")
+
+    def df_get(self, subset, clustering, coords):
+        df = pd.DataFrame({
+            "Subset" : np.repeat(subset, len(clustering)),
+            "Clustering" : clustering,
+            "UMAP 1" : coords[:, 0],
+            "UMAP 2" : coords[:, 1]
+        })
+        return df
+
+    def umap_df(self):
+        subset_list = [
+            "harmony",
+            "scvi",
+            "bbknn",
+            "scanorama",
+            "seurat"
+        ]
+        clustering_list = [
+            self.clustering_harmony,
+            self.clustering_scvi,
+            self.clustering_bbknn,
+            self.clustering_scanorama,
+            self.clustering_seurat
+        ]
+        coords_list = [
+            self.umap_harmony,
+            self.umap_scvi,
+            self.umap_bbknn,
+            self.umap_scanorama,
+            self.umap_seurat
+        ]
+
+        umap_dfs = [
+            self.df_get(i, j, k) for i, j, k in zip(
+                subset_list,
+                clustering_list,
+                coords_list
+            )
+        ]
+        self.umap_concat = pd.concat(umap_dfs)
+
+    def umap_plot(self, show_plot = False):
+        self.umap_df()
+        if len(np.unique(self.umap_concat["Clustering"])) > 20:
+            palette = cc.glasbey_bw[0:len(np.unique(self.umap_concat["Clustering"]))]
+        else:
+            palette = "tab20"
+        self.umap_plt = sns.FacetGrid(
+            self.umap_concat,
+            col = "Subset",
+            col_wrap = 3,
+            hue = "Clustering",
+            palette = palette
+        )
+        self.umap_plt.map(
+            sns.scatterplot,
+            "UMAP 1",
+            "UMAP 2",
+            s = 5,
+            alpha = 0.5
+        )
+        self.umap_plt.add_legend(markerscale = 3, title = self.subset_name)
+        if show_plot is True:
+            return self.umap_plt                
+
+    def save_umap(self, save_dir, dpi = 300):
+        try:
+            self.umap_plt.savefig(
+                save_dir,
+                dpi = dpi
+            )
+        except:
+            self.umap_plot()
+            self.umap_plt.savefig(
+                save_dir,
+                dpi = dpi
+            )
