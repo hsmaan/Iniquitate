1c92473395b4cae08990ee2d1d7bf4394cfebbd8
hsmaan <hassaan.maan@mail.utoronto.ca>
2023-05-15 20:24:15 -0400

    added r analysis script for umap results

---
+++ workflow/analysis/R/21_PBMC_perturbation_umap_plots.R
@@ -0,0 +1,164 @@
+library(data.table)
+library(tidyverse)
+library(reshape2)
+library(ggplot2)
+library(ggthemes)
+library(ggExtra)
+library(ggpubr)
+library(dotwhisker)
+library(Seurat)
+library(SeuratDisk)
+library(ComplexHeatmap)
+library(circlize)
+library(RColorBrewer)
+library(Cairo)
+
+# Helper functions
+`%ni%` <- Negate(`%in%`)
+
+# Change to results dir for uamp results data
+setwd("../../../results/umap/")
+
+# Load color palette 
+kev_palette <- c("dodgerblue2", 
+                 "#E31A1C",
+                 "green4",
+                 "#6A3D9A", 
+                 "#FF7F00", 
+                 "black",
+                 "gold1",
+                 "skyblue2",
+                 "#FB9A99", 
+                 "palegreen2",
+                 "#CAB2D6", 
+                 "#FDBF6F", 
+                 "gray70", 
+                 "khaki2",
+                 "maroon",
+                 "orchid1",
+                 "deeppink1",
+                 "blue1",
+                 "steelblue4",
+                 "darkturquoise",
+                 "green1",
+                 "yellow4",
+                 "yellow3",
+                 "darkorange4",
+                 "brown")
+
+##### Analysis of PBMC 2 batch balanced data - baseline #####
+
+# Load in the umap plot results 
+setwd("umap_plots/")
+umap_files <- list.files()
+umap_files <- grep(
+  ".tsv",
+  umap_files,
+  value = TRUE
+)
+umap_files <- grep(
+  "pbmc_2_batch_base_balanced",
+  umap_files,
+  value = TRUE
+)
+umap_loaded <- lapply(umap_files, fread)
+umap_names <- str_split_fixed(umap_files, fixed(".tsv"), 2)[,1]
+names(umap_loaded) <- umap_names
+
+setwd("../../..")
+
+# Create directory for umap results if it doesn't exist
+if (!dir.exists("outs/umap/results")) {
+  dir.create("outs/umap/results", recursive = TRUE)
+}
+if (!dir.exists("outs/umap/figures")) {
+  dir.create("outs/umap/figures")
+}
+
+# Create function to loop over the umap files and return the results 
+umap_plot <- function(df, save_prefix) {
+  unique_cluster_len <- length(unique(df$Clustering))
+  if (unique_cluster_len > 8) {
+    ggplot(data = df, aes(x = `UMAP 1`, y = `UMAP 2`)) +
+      geom_point(
+        aes(
+          color = factor(Clustering),
+        )
+      ) +
+      facet_wrap(
+        .~Subset, 
+        scales = "free_x"
+      ) +
+      labs(
+        color = "",
+        x = "UMAP 1",
+        y = "UMAP 2"
+      ) +
+      scale_color_manual(
+        name = "",
+        values = kev_palette[1:unique_cluster_len]
+      ) +  
+      theme_few() +
+      theme(axis.title.x = element_text(size = 16)) +
+      theme(axis.title.y = element_text(size = 16)) +
+      theme(strip.text.x = element_text(size = 16)) +
+      theme(plot.title = element_text(size = 14)) +
+      theme(axis.text.x = element_text(size = 16)) +
+      theme(axis.text.y = element_text(size = 16)) +
+      theme(legend.title = element_text(size = 16)) +
+      theme(legend.text = element_text(size = 16))
+    ggsave(
+      paste0(
+        "outs/umap/figures/",
+        save_prefix,
+        ".pdf"
+      ),
+      width = 12,
+      height = 8,
+      device = cairo_pdf
+    )
+  } else {
+    ggplot(data = df, aes(x = `UMAP 1`, y = `UMAP 2`)) +
+      geom_point(
+        aes(
+          color = factor(Clustering),
+        )
+      ) +
+      facet_wrap(
+        .~Subset, 
+        scales = "free_x"
+      ) +
+      labs(
+        color = "",
+        x = "UMAP 1",
+        y = "UMAP 2"
+      ) +
+      scale_color_brewer(palette = "Dark2") +  
+      theme_few() +
+      theme(axis.title.x = element_text(size = 16)) +
+      theme(axis.title.y = element_text(size = 16)) +
+      theme(strip.text.x = element_text(size = 16)) +
+      theme(plot.title = element_text(size = 14)) +
+      theme(axis.text.x = element_text(size = 16)) +
+      theme(axis.text.y = element_text(size = 16)) +
+      theme(legend.title = element_text(size = 16)) +
+      theme(legend.text = element_text(size = 16))
+    ggsave(
+      paste0(
+        "outs/umap/figures/",
+        save_prefix,
+        ".pdf"
+      ),
+      width = 12,
+      height = 8,
+      device = cairo_pdf
+    )
+  }
+}
+
+# Iterate over the umap files and namees and save the results 
+mapply(
+  umap_plot,
+  df = umap_loaded, 
+  save_prefix = umap_names
+)
